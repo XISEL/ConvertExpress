42491e37dda160052e4b6a938a4e6e41bb69919c
glinomes69 <120213287+glinomes69@users.noreply.github.com>
2024-03-06 22:50:15 +0300

    Add files via upload

--- ConvertExpress/ConvertExpress/MainWindow.xaml.cs
+++ ConvertExpress/ConvertExpress/MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Threading;
 
-namespace ConvertExpress
+namespace CurrencyParser
 {
+    /// <summary>
     /// Главное окно приложения
+    /// </summary>
     public partial class MainWindow : Window
     {
         // Таймер для обновления времени
@@ -21,6 +27,12 @@ namespace ConvertExpress
 
             // Установка DataContext для привязки данных
             DataContext = this;
+
+            // Привязка обработчика события изменения текста в TextBox
+            inputPriceTo.TextChanged += TextBox_TextChanged;
+
+            // Загрузка данных о валютах
+            LoadCurrencyData();
 
             // Установка значения по умолчанию для TextBox
             inputPriceTo.Text = "1";
@@ -46,7 +58,170 @@ namespace ConvertExpress
             // Обновление содержимого TextBlock
             TimeTextbox.Text = time;
         }
-        
+
+        // Список объектов CurrencyItem для хранения данных о валютах
+        private List<CurrencyItem> _currencyItems;
+
+        // Свойство для доступа к списку валют
+        public List<CurrencyItem> CurrencyItems
+        {
+            get { return _currencyItems; }
+            set
+            {
+                _currencyItems = value;
+                OnPropertyChanged("CurrencyItems");
+            }
+        }
+
+        // Список объектов CurrencyItem для хранения данных о валютах для исходной валюты
+        public List<CurrencyItem> FromCode { get; set; }
+
+        // Список объектов CurrencyItem для хранения данных о валютах для целевой валюты
+        public List<CurrencyItem> FromCurrencyItems { get; set; }
+        public List<CurrencyItem> ToCurrencyItems { get; set; }
+
+        // Метод для загрузки данных о валютах
+        private void LoadCurrencyData()
+        {
+            // Получение данных о валютах с использованием парсера
+            CurrencyItems = CurrencyParser1.ParseCurrencies();
+
+            // Добавление рубля в список валют
+            CurrencyItems.Add(new CurrencyItem { Code = "RUB", Name = "Рубль", Rate = "1" });
+
+            // Создание отсортированных списков для каждого ComboBox
+            FromCurrencyItems = CurrencyItems.OrderBy(currency => currency.Name)
+                                              .Select(currency => new CurrencyItem
+                                              {
+                                                  Name = $"{currency.Name} ({currency.Code})",
+                                                  Code = currency.Code,
+                                                  Rate = currency.Rate
+                                              })
+                                              .ToList();
+            ToCurrencyItems = CurrencyItems.OrderBy(currency => currency.Name)
+                                            .Select(currency => new CurrencyItem
+                                            {
+                                                Name = $"{currency.Name} ({currency.Code})",
+                                                Code = currency.Code,
+                                                Rate = currency.Rate
+                                            })
+                                            .ToList();
+        }
+
+        // Обработчик изменения выбранной валюты в ComboBox
+        private void CurrencyComboBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateExchangeRate();
+        }
+
+        // Метод для обновления курса обмена
+        private void UpdateExchangeRate()
+        {
+            // Проверка выбраны ли валюты для обмена
+            if (fromCurrencyComboBox.SelectedItem != null && toCurrencyComboBox.SelectedItem != null)
+            {
+                // Извлечение объектов CurrencyItem для выбранных валют
+                CurrencyItem fromCurrencyItem = (CurrencyItem)fromCurrencyComboBox.SelectedItem;
+                CurrencyItem toCurrencyItem = (CurrencyItem)toCurrencyComboBox.SelectedItem;
+
+                // Извлечение кодов валют из объектов CurrencyItem
+                string fromCurrencyCode = fromCurrencyItem.Code;
+                string toCurrencyCode = toCurrencyItem.Code;
+
+                // Извлечение курсов обмена для выбранных валют
+                string fromRate = fromCurrencyComboBox.SelectedValue.ToString();
+                string toRate = toCurrencyComboBox.SelectedValue.ToString();
+
+                // Проверка, удалось ли извлечь курсы обмена
+                if (double.TryParse(fromRate, out double fromValue) && double.TryParse(toRate, out double toValue))
+                {
+                    // Проверка, корректен ли введенный пользователем текст в TextBox
+                    string check = inputPriceTo.Text;
+                    bool isDouble = double.TryParse(check, out _);
+                    if (isDouble == true && Convert.ToDouble(inputPriceTo.Text) > -1)
+                    {
+                        // Вычисление курса обмена и обновление интерфейса
+                        double inputPriceToConvert = Convert.ToDouble(inputPriceTo.Text);
+                        double exchangeRate = toValue / fromValue;
+                        double convertedAmount = inputPriceToConvert * exchangeRate;
+                        exchangeRateTextBlock.Text = $"{convertedAmount:F4}";
+
+                        // Вычисление курса для одной единицы валюты и обновление интерфейса
+                        double reverseExchangeRate = 1 / exchangeRate;
+                        double OnePriceExchangeRate = exchangeRate;
+                        PriceShowOne.Text = $"1 {toCurrencyCode} = {OnePriceExchangeRate:F4} {fromCurrencyCode}";
+                        PriceShowSecond.Text = $"1 {fromCurrencyCode} = {reverseExchangeRate:F4} {toCurrencyCode}";
+                    }
+                    else
+                    {
+                        // Вывод сообщения об ошибке в случае некорректного ввода
+                        exchangeRateTextBlock.Text = $"???";
+                    }
+                }
+            }
+        }
+
+        // Метод для получения кода валюты по ее названию
+        private string GetCurrencyCode(string currencyName)
+        {
+            // Поиск объекта CurrencyItem по имени валюты и возврат свойства Code
+            CurrencyItem currencyItem = CurrencyItems.FirstOrDefault(item => item.Name == currencyName);
+            return currencyItem != null ? currencyItem.Code : string.Empty;
+        }
+
+        // Обработчик нажатия кнопки для обмена выбранных валют местами
+        private void SwapCurrenciesButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Обмен местами выбранных валют
+            int fromIndex = fromCurrencyComboBox.SelectedIndex;
+            fromCurrencyComboBox.SelectedIndex = toCurrencyComboBox.SelectedIndex;
+            toCurrencyComboBox.SelectedIndex = fromIndex;
+            string fromRate = fromCurrencyComboBox.SelectedValue.ToString();
+            string toRate = toCurrencyComboBox.SelectedValue.ToString();
+            CurrencyItem fromCurrencyItem = (CurrencyItem)fromCurrencyComboBox.SelectedItem;
+            CurrencyItem toCurrencyItem = (CurrencyItem)toCurrencyComboBox.SelectedItem;
+
+            // Извлечение кодов валют из объектов CurrencyItem
+            string fromCurrencyCode = fromCurrencyItem.Code;
+            string toCurrencyCode = toCurrencyItem.Code;
+
+            // Проверка, удалось ли извлечь курсы обмена
+            if (double.TryParse(fromRate, out double fromValue) && double.TryParse(toRate, out double toValue))
+            {
+                // Проверка, корректен ли введенный пользователем текст в TextBox
+                string check = inputPriceTo.Text;
+                bool isDouble = double.TryParse(check, out _);
+                if (isDouble == true && Convert.ToDouble(inputPriceTo.Text) > -1)
+                {
+                    // Вычисление курса обмена и обновление интерфейса
+                    double inputPriceToConvert = Convert.ToDouble(inputPriceTo.Text);
+                    double exchangeRate = toValue / fromValue;
+                    double convertedAmount = inputPriceToConvert * exchangeRate;
+                    exchangeRateTextBlock.Text = $"{convertedAmount:F4}";
+
+                    // Вычисление курса для одной единицы валюты и обновление интерфейса
+                    double reverseExchangeRate = 1 / exchangeRate;
+                    double OnePriceExchangeRate = exchangeRate;
+                    PriceShowOne.Text = $"1 {toCurrencyCode} = {OnePriceExchangeRate:F4} {fromCurrencyCode}";
+                    PriceShowSecond.Text = $"1 {fromCurrencyCode} = {reverseExchangeRate:F4} {toCurrencyCode}";
+                }
+                else
+                {
+                    // Вывод сообщения об ошибке в случае некорректного ввода
+                    exchangeRateTextBlock.Text = $"???";
+                }
+            }
+        }
+
+        // Обработчик изменения текста в TextBox
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Обновление курса обмена при изменении текста в TextBox
+            if (fromCurrencyComboBox.SelectedItem != null && toCurrencyComboBox.SelectedItem != null)
+            {
+                UpdateExchangeRate();
+            }
+        }
 
         // Обработчик ввода текста в TextBox
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -60,6 +235,15 @@ namespace ConvertExpress
                     return;
                 }
             }
+        }
+
+        // Событие, вызываемое при изменении свойства объекта
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Метод для вызова события изменения свойства
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
